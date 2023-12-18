7f0d1ae00b02361b8c51465039bdaf9430f4910d
Saeedx303 <130200805+Saeedx303@users.noreply.github.com>
2023-12-19 02:43:37 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,13 +7,13 @@ global.autoReadAll = true
 global.antitags = true
 
 
-global.Owner = ['6283866366437']
-global.OwnerNumber = ['6283866366437']
-global.ownertag = ['6283866366437']
-global.BotName = "Siesta Botz"
-global.packname = "Siesta Botz"
-global.author = "By: Kanna Chan"
-global.OwnerName = "Kanna"
+global.Owner = ['923361501800']
+global.OwnerNumber = ['923361501800']
+global.ownertag = ['923361501800']
+global.BotName = "BABLI Botz"
+global.packname = "BABLI Botz"
+global.author = "By: BABLI"
+global.OwnerName = "BABLI×BABLI"
 global.BotSourceCode = "Beli Sc Nya Lah Anjay🐤🐤"
 global.SupportGroupLink = "https://chat.whatsapp.com/KNuPWvGn3AG0srT3vdr0Ea"
 global.sessionName = "session"
@@ -24,8 +24,8 @@ global.prefa = ['-']
 
 
 global.location = "West Bengal, Indonesia"
-global.reactmoji = "❤️"
-global.themeemoji = "💖"
+global.reactmoji = "🐸"
+global.themeemoji = "🖕🏿"
 global.vidmenu = { url: 'https://c.tenor.com/YGuLegQWubwAAAPo/miku-nakano-gotoubun-no-hanayome.mp4' }
 global.websitex = "https://github.com/XyzenzKannaV1"
 global.lolhuman = "Siesta"
@@ -59,15 +59,15 @@ global.ntilink = []
 
 global.mess = {
     jobdone: 'Job done...',
-    useradmin: 'Sorry, only *Group Admins* can use this command *Baka*!',
-    botadmin: 'Sorry, i cant execute this command without being an *Admin* of this group.',
+    useradmin: 'Sorry, only *BABLI* can use this command *Baka*!',
+    botadmin: 'Sorry, i cant execute this command without being an *BABLI* of this group.',
     botowner: 'Only my *Owner* can use this command, Baka!',
-    grouponly: 'This command is only made for *Groups*, Baka!',
+    grouponly: 'This command is only made for *BABLI*, Baka!',
     privateonly: 'This command is only made for *Private Chat*, Baka!',
     botonly: 'Only the *Bot itself* can use this command!',
     waiting: 'Chotto Matte...',
     nolink: 'Please provide me *link*, Baka!',
-    error: 'An error occurd!',
+    error: 'Sorry 🙏!',
     banned: 'You are *Banned* fron using commands!',
     bangc: 'This Group is *Banned* from using Commands!',
     nonsfw: 'Dont be a pervert Baka! This is not a NSFW enabled group!'
@@ -77,7 +77,7 @@ global.mess = {
 
 global.limitawal = {
     premium: "Infinity",
-    free: 2,
+    free: Infinity,
     monayawal: 1000
 }
    global.rpg = {
